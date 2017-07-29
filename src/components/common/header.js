0631cd3c5b3a6e65ba66eeab144aec5c34eec2a7
Christopher N. KATOYI-KABA <christopher.katoyi@icloud.com>
2017-07-29 22:00:14 +0200

    Common components

---
+++ src/components/common/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export const Header = ({ title }) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{title}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F8F8F8',
+        height: 70,
+        padding: 15,
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.2,
+        position: 'relative',
+    },
+    textStyle: {
+        paddingTop: 15,
+        fontSize: 20
+    }
+};
